1eb7405714c0eac87027407bc5ae5303355225ae
mo <jadaa@gmail.com>
2025-12-04 23:37:23 +0200

    Fix mysql2 loading for Vercel

--- config/config.js
+++ config/config.js
@@ -24,6 +24,7 @@ module.exports = {
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         dialect: "mysql",
+        dialectModule: require('mysql2'),
         dialectOptions: {
             ssl: {
                 require: true,
