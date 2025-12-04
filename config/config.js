1aad24b768a7bffa74b775fb5fb15c9dc5f9d02d
mo <jadaa@gmail.com>
2025-12-05 00:54:42 +0200

    Remove XAMPP config and temporary scripts

--- config/config.js
+++ config/config.js
@@ -2,12 +2,19 @@ require('dotenv').config();
 
 module.exports = {
     development: {
-        username: "root",
-        password: "",
-        database: "jadaamart_db",
-        host: "127.0.0.1",
-        port: 3307,
-        dialect: "mysql"
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "mysql",
+        dialectModule: require('mysql2'),
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
     },
     test: {
         username: process.env.DB_USERNAME,
