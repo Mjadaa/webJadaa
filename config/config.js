17a10ff22396ff74a742e3c547981193f07efe27
mo <jadaa@gmail.com>
2025-12-05 01:11:09 +0200

    Remove unused PHP directories and update package.json

--- config/config.js
+++ config/config.js
@@ -2,19 +2,13 @@ require('dotenv').config();
 
 module.exports = {
     development: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
+        username: "root",
+        password: "",
+        database: "jadaamart_db",
+        host: "127.0.0.1",
+        port: 3307,
         dialect: "mysql",
-        dialectModule: require('mysql2'),
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        }
+        dialectModule: require('mysql2')
     },
     test: {
         username: process.env.DB_USERNAME,
