1c26799661058824c9dd91d3f1666fd9816cd273
mo <jadaa@gmail.com>
2025-12-04 23:04:11 +0200

    Fix model loading for Vercel

--- models/index.js
+++ models/index.js
@@ -16,20 +16,14 @@ if (config.use_env_variable) {
     sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs
-    .readdirSync(__dirname)
-    .filter(file => {
-        return (
-            file.indexOf('.') !== 0 &&
-            file !== basename &&
-            file.slice(-3) === '.js' &&
-            file.indexOf('.test.js') === -1
-        );
-    })
-    .forEach(file => {
-        const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-        db[model.name] = model;
-    });
+// Manual import for Serverless/Vercel compatibility
+db.User = require('./user')(sequelize, Sequelize.DataTypes);
+db.Category = require('./category')(sequelize, Sequelize.DataTypes);
+db.Product = require('./product')(sequelize, Sequelize.DataTypes);
+db.Cart = require('./cart')(sequelize, Sequelize.DataTypes);
+db.CartItem = require('./cart_item')(sequelize, Sequelize.DataTypes);
+db.Order = require('./order')(sequelize, Sequelize.DataTypes);
+db.OrderItem = require('./order_item')(sequelize, Sequelize.DataTypes);
 
 Object.keys(db).forEach(modelName => {
     if (db[modelName].associate) {
