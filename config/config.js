087b9400ff28f40130b52a50b88ec5f2d1f3df1d
mo <jadaa@gmail.com>
2025-12-04 23:24:34 +0200

    Fix DB_USERNAME mismatch

--- config/config.js
+++ config/config.js
@@ -18,7 +18,7 @@ module.exports = {
         dialect: "mysql"
     },
     production: {
-        username: process.env.DB_USERNAME,
+        username: process.env.DB_USERNAME || process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
